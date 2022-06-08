6102ade5fb73bc5532658a96fcb17f41639e7579
hackingyseguridad <antonio_taboada@hotmail.es>
2022-06-08 16:04:21 +0200

    Create instalar.sh

---
+++ instalar.sh
@@ -0,0 +1,8 @@
+apt-get install nmap
+apt-get install 
+apt-get install uniscan
+apt-get install docutils git perl map sslscan
+git clone https://github.com/golismero/golismero.git
+cd golismero
+pip install -r requirements.txt
+cd ..
