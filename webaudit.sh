5dafe561c988738b0a804664806c22d9a598aa73
hackingyseguridad <antonio_taboada@hotmail.es>
2023-10-04 11:26:32 +0200

    Update webaudit.sh

--- webaudit.sh
+++ webaudit.sh
@@ -118,7 +118,7 @@ golismero -e brute_dns scan $1
 echo
 echo -e "\e[00;32m# Descubre directorios por fuerza bruta ########################################################\e[00m" 
 echo
-dirb http://$1 -fi diccionario.txt -M 100,204,307,400,401,403,409,500,503 -f -w  -z 99 -a "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8"
+dirb http://$1 diccionario.txt -M 100,204,307,400,401,403,409,500,503 -f -w  -z 99 -a "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" -H "Accept: text/html, applicattion/xhtml+xml, application/xml;q=0.9,*/*;q=0.8"
 echo
 echo -e "\e[00;32m# Vulnerable a XSS ########################################################\e[00m" 
 echo
