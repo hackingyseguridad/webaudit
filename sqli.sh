408f6c312886715929e1984d3a13b3f4c5519a2e
hackingyseguridad <antonio_taboada@hotmail.es>
2025-12-10 11:22:28 +0100

    Update sqli.sh

--- sqli.sh
+++ sqli.sh
@@ -1,4 +1,5 @@
-
+#!/bin/sh
+cat << "INFO"
 
 ███████╗ ██████╗ ██╗      ██╗
 ██╔════╝██╔═══██╗██║      ██║
@@ -6,15 +7,16 @@
 ╚════██║██║   ██║██║      ██║
 ███████║╚██████╔╝███████╗ ██║
 ╚══════╝ ╚═════╝ ╚══════╝ ╚═╝
-
+                             http://www.hackingyseguridad.com
+INFO
+if [ -z "$1" ]; then
+echo
 # --dbs  identifica bases de datos disponibles: "
 # --level=5 --risk=3 probar todos los parametros:"
 # --banner información del banner:"
-
 echo 
 echo "Uso.: ./sqli.sh URL
 echo "URL p.ej.: http://hackingyseguridad.com/?s=variable"
 echo 
-
 
 sqlmap -u $1 $2 --level=5 --risk=3 --batch --dbs --banner
