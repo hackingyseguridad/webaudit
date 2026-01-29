ae52a37079114794e77f5b47b001c989a6867a27
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-29 09:46:06 +0100

    Update secretos.sh

--- secretos.sh
+++ secretos.sh
@@ -1,17 +1,12 @@
-# busca en el codigo secretos: credenciales, tokens, claves
+#
+# busca online en el codigo secretos: credenciales, tokens, claves
+# uso.: secretos.sh URL 
+# cat index.html | grep -aoP "(?<=(\"|\'|\`))\/[a-zA-Z0-9_?&=\/\-\#\.]*(?=(\"|\'|\`))" | sort -u
+# hackingyseguridad.com 2026
+# @antonio_taboada
 
-cat index.html | grep -aoP "(?<=(\"|\'|\`))\/[a-zA-Z0-9_?&=\/\-\#\.]*(?=(\"|\'|\`))" | sort -u
+curl -s $1 $2 | grep -aoP "(?<=(\"|\'|\`))\/[a-zA-Z0-9_?&=\/\-\#\.]*(?=(\"|\'|\`))" | sort -u
+curl -s $1 $2 | grep -Ei "user|token|password|auth|api|sql|digest|email|oauth2"
 
-cat index.html | grep  -Hrn "user" 
-cat index.html | grep  -Hrn "http*" 
-cat index.html | grep  -Hrn "token*" 
-cat index.html | grep  -Hrn "password*" 
-cat index.html | grep  -Hrn "auth" 
-cat index.html | grep  -Hrn "api" 
-cat index.html | grep  -Hrn "sql" 
-cat index.html | grep  -Hrn "Digest" 
-cat index.html | grep  -Hrn "email" 
-cat index.html | grep  -Hrn "ouath2" 
-cat index.html | grep  -Hrn "UserName" 
-cat index.html | grep  -Hrn "Passorwd" 
-cat index.html | grep  -Hrn "User" 
+
+
